7b53970692622323683a34eeba31259321cd5b39
SnowCoder404 <contact@marco-lenschau.de>
2025-05-14 00:29:14 +0200

    Add input for different input fields.

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-input',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './input.component.html',
   styleUrl: './input.component.scss'
 })
 export class InputComponent {
+  @Input() type: string = '';
 
+  getPlaceholder(): string {
+    if (this.type === "email") {
+      return "max-mustmann@mustermail.com";
+    } else if (this.type === "password") {
+      return "Password";
+    } else {
+      return "";
+    }
+  }
 }
