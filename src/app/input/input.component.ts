c3f44a8e5ebeaa5a2f22088af9abafccc0b61945
SnowCoder404 <contact@marco-lenschau.de>
2025-05-14 01:05:19 +0200

    Finish the input component

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -15,6 +15,14 @@ export class InputComponent {
       return "max-mustmann@mustermail.com";
     } else if (this.type === "password") {
       return "Password";
+    } else if (this.type === "new-password") {
+      return "New password";
+    }  else if (this.type === "retry-password") {
+      return "Retry password";
+    } else if (this.type === "channel-name") {
+      return "Project";
+    } else if (this.type === "description") {
+      return "Your text";
     } else {
       return "";
     }
