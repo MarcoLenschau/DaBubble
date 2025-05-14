d2c4b805140e35633c03fe8991f8ee3998964c08
SnowCoder404 <contact@marco-lenschau.de>
2025-05-14 02:29:41 +0200

    Improve sign up page

---
+++ src/app/sign-up/sign-up.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { InputComponent } from '../input/input.component';
+
+@Component({
+  selector: 'app-sign-up',
+  imports: [InputComponent],
+  templateUrl: './sign-up.component.html',
+  styleUrl: './sign-up.component.scss'
+})
+export class SignUpComponent {
+
+}
