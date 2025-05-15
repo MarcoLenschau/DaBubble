62bf3e60daab302994988ed2202dfeb910d1e403
Martin Sinner <m-sinner@web.de>
2025-05-16 00:12:24 +0200

    added fonts, changed background color in main part, added devspace bar

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { DevspaceComponent } from './devspace/devspace.component';
 
 @Component({
   selector: 'app-main',
-  imports: [],
+  imports: [DevspaceComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
