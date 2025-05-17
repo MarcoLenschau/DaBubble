eaa123cc13d2333597deb6d5bbf212fecb4464b4
Martin Messirek <145527297+Martin-Messirek@users.noreply.github.com>
2025-05-17 21:23:48 +0200

    Improve thread.component structure with ngIf and ngClass

--- src/app/main/thread/thread.component.ts
+++ src/app/main/thread/thread.component.ts
@@ -1,11 +1,57 @@
+import { CommonModule, NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-thread',
-  imports: [],
+  imports: [NgClass, CommonModule],
   templateUrl: './thread.component.html',
-  styleUrl: './thread.component.scss'
+  styleUrl: './thread.component.scss',
 })
 export class ThreadComponent {
+  hovered = false;
 
+  resetHoverState() {
+    this.hovered = false;
+  }
+
+  setHoverState(index: number, state: boolean) {
+    this.messages.forEach((msg, i) => {
+      msg.hovered = i === index ? state : false;
+    });
+  }
+
+  messages = [
+    {
+      name: 'Noah Braun',
+      time: '14:24 Uhr',
+      text: 'Hallo zusammen, wie läuft das aktuelle Projekt bei euch?',
+      isSelf: false,
+      isThreadStarter: true,
+      hovered: false,
+    },
+    {
+      name: 'Sofia Müller',
+      time: '15:31 Uhr',
+      text: 'Bei uns läuft alles gut, wir sind fast mit der neuen Funktion fertig.',
+      isSelf: false,
+      isThreadStarter: false,
+      hovered: false,
+    },
+    {
+      name: 'Anna Müller',
+      time: '15:37 Uhr',
+      text: 'Super, dann können wir ja bald mit dem Testing starten! Lorem ipsum dolor sit, amet consectetur adipisicing elit. ',
+      isSelf: false,
+      isThreadStarter: false,
+      hovered: false,
+    },
+    {
+      name: 'Frederik Beck',
+      time: '15:44 Uhr',
+      text: 'Ja genau.',
+      isSelf: true,
+      isThreadStarter: false,
+      hovered: false,
+    },
+  ];
 }
