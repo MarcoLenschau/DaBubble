f347579977c8d04c1252638f2aab3680b39cea51
Emrah3865 <81292068+Emrah3865@users.noreply.github.com>
2025-05-17 12:56:45 +0200

    Message hmtl and scss

--- src/app/main/message/message.component.ts
+++ src/app/main/message/message.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+
+import {Component} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-message',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule,FormsModule],
   templateUrl: './message.component.html',
   styleUrl: './message.component.scss'
 })
