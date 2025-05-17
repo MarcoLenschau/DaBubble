48c2c8477b624f8384307e797f6e097e8c48be5d
SnowCoder404 <contact@marco-lenschau.de>
2025-05-18 00:22:32 +0200

    Add log out funktion

--- src/app/dialogs/dialog-user-options/dialog-user-options.component.ts
+++ src/app/dialogs/dialog-user-options/dialog-user-options.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { RouterService } from '../../services/router.service';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-dialog-user-options',
@@ -7,5 +9,10 @@ import { Component } from '@angular/core';
   styleUrl: './dialog-user-options.component.scss'
 })
 export class DialogUserOptionsComponent {
+  constructor(private router: RouterService, private dialogRef: MatDialogRef<DialogUserOptionsComponent>) {}
 
+  logOut() {
+    this.router.switchRoute("");
+    this.dialogRef.close();
+  }
 }
