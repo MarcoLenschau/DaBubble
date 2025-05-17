e9748c2d10609105e8e3993142447fd2b364b0b3
Emrah3865 <81292068+Emrah3865@users.noreply.github.com>
2025-05-17 12:56:10 +0200

    Message Html and Scss

--- src/app/main/message/message.component.ts
+++ src/app/main/message/message.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-message',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './message.component.html',
   styleUrl: './message.component.scss'
 })
