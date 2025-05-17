797304a90567b608a7582c57a0585cbed909fc30
SnowCoder404 <contact@marco-lenschau.de>
2025-05-17 18:09:00 +0200

    Change router in router outlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import {Component} from '@angular/core';
-import { Router } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { MessageComponent } from "./main/message/message.component";
 
 @Component({
   selector: 'app-root',
-  imports: [Router, HeaderComponent, FooterComponent, MessageComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, MessageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
