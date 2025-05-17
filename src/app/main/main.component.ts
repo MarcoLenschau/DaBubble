188dffbd343dc250440ca3671f98239b4bdedddb
Martin Messirek <145527297+Martin-Messirek@users.noreply.github.com>
2025-05-17 10:44:14 +0200

    Style: main.component

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { DevspaceComponent } from './devspace/devspace.component';
+import { ThreadComponent } from './thread/thread.component';
+import { MessageComponent } from './message/message.component';
 
 @Component({
   selector: 'app-main',
-  imports: [DevspaceComponent],
+  imports: [DevspaceComponent, MessageComponent, ThreadComponent],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.scss'
+  styleUrl: './main.component.scss',
 })
-export class MainComponent {
-
-}
+export class MainComponent {}
