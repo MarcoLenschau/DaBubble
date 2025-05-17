6ffe5d79deddb3429cbb481d271a1b00a70746f9
SnowCoder404 <contact@marco-lenschau.de>
2025-05-18 00:06:41 +0200

    Add user options dialog at header

---
+++ src/app/dialogs/dialog-user-options/dialog-user-options.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-user-options',
+  imports: [],
+  templateUrl: './dialog-user-options.component.html',
+  styleUrl: './dialog-user-options.component.scss'
+})
+export class DialogUserOptionsComponent {
+
+}
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -2,6 +2,8 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterService } from '../../services/router.service';
 import { InputComponent } from '../../input/input.component';
+import { DialogUserOptionsComponent } from '../../dialogs/dialog-user-options/dialog-user-options.component';
+import {MatDialog} from '@angular/material/dialog'
 
 @Component({
   selector: 'app-header',
@@ -10,6 +12,9 @@ import { InputComponent } from '../../input/input.component';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-  constructor(public router: RouterService) {}
+  constructor(private dialog: MatDialog,public router: RouterService) {}
 
+  openDialog() {
+      this.dialog.open(DialogUserOptionsComponent);
+  }
 }
