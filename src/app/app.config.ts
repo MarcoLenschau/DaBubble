237e39a5a01e28ab370e1185c01eb0271fc6ff75
Marco Lenschau <contact@marco-lenschau.de>
2025-05-22 01:56:47 +0200

    Add firebase backend and the default funktion for load data

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,9 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "dabubble-78e7f", appId: "1:245552915538:web:0de528037a152f6b58ab8b", storageBucket: "dabubble-78e7f.firebasestorage.app", apiKey: "AIzaSyDIEYY0u607-Lf9mnEYF3pFVh3BFNZ3k2c", authDomain: "dabubble-78e7f.firebaseapp.com", messagingSenderId: "245552915538" })), provideFirestore(() => getFirestore())]
 };
--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,10 +1,23 @@
-import { Injectable, inject } from '@angular/core';
-import { Firestore, collection, doc, onSnapshot, addDoc, updateDoc, deleteDoc } from '@angular/fire/firestore';
+import { Injectable} from '@angular/core';
+import { Firestore, collection, collectionData, doc, onSnapshot, addDoc, updateDoc, deleteDoc } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FirebaseService {
 
-  constructor(private firebase: Firestore ) { }
+  constructor(private firebase: Firestore ) {}
+
+  getDocRef(docRef: string) {
+    return collection(this.firebase, docRef);
+  }
+
+  getColRef(col: string) {
+    return collectionData(this.getDocRef(col),{ idField: 'id' });
+  }
+
+  getSingleDocRef(docRef: string, docId: any) {
+    return doc(this.firebase, docRef, docId);
+  }
+
 }
