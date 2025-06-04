1d37f1f2343f1b488f7f3f8aa144feaf61d19ce8
MartinSinner <m-sinner@web.de>
2025-06-04 20:38:42 +0200

    added channel details component, overlay design

---
+++ src/app/main/message-panel/messages-header/channel-details-overlay/channel-details-overlay.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Input } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-channel-details-overlay',
+  imports: [],
+  templateUrl: './channel-details-overlay.component.html',
+  styleUrl: './channel-details-overlay.component.scss'
+})
+export class ChannelDetailsOverlayComponent {
+  @Input() channel: any;
+  
+  close: any;
+
+}
