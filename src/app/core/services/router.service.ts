7226060e7b2c940da43417960a7dad3b6dc6d10a
Marco Lenschau <contact@marco-lenschau.de>
2025-06-28 17:29:28 +0200

    doc: Add documentaction for router sevice

--- src/app/core/services/router.service.ts
+++ src/app/core/services/router.service.ts
@@ -1,22 +1,37 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RouterService {
-
-  constructor(private router: Router) { }
-
-  switchRoute(route: string) {
+  private router = inject(Router);
+  
+  /**
+   * Navigates to the specified route using Angular Router.
+   *
+   * @param {string} route - The route path to navigate to.
+   * @return {void} This function does not return a value.
+   */
+  switchRoute(route: string): void {
     this.router.navigate([route]);
   }
 
-  isSignInPage() {
+  /**
+   * Checks if the current URL is the sign-in page ("/").
+   *
+   * @return {boolean} Returns true if the current route is the sign-in page, otherwise false.
+   */
+  isSignInPage(): Boolean {
     return this.router.url == "/";
   }
 
-  isMainPage() {
+  /**
+   * Checks if the current URL is the main message page ("/message").
+   *
+   * @return {boolean} Returns true if the current route is the main message page, otherwise false.
+   */
+  isMainPage(): Boolean {
     return this.router.url == "/message";
   }
 }
