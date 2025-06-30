413d20f65fa1e8423d91bb84435f7bafda3a1316
Marco Lenschau <contact@marco-lenschau.de>
2025-06-30 03:10:57 +0200

    refactor: Add maximum lines of files in the esline rules

--- eslint.config.js
+++ eslint.config.js
@@ -15,6 +15,7 @@ export default defineConfig([
     },
     rules: {
       "max-lines-per-function": ["error", {max: 14, skipBlankLines: true, skipComments: true},],
+      "max-lines": ["error", { max: 300 }],
       "semi": ["error", "always"],
       "no-console": ["error"],
       "prefer-const": ["warn"],
